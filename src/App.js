e96dab62a6780eca635646dd3d2e889a8ed779f0
Ethan Frigon <ethan.frigon@gmail.com>
2019-06-23 18:11:06 -0700

    added router and redux and set up overall framework

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Route, withRouter} from 'react-router-dom';
+import {connect} from 'react-redux';
+import LandingPage from './components/landing-page/LandingPage';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component {
+  render(){
+    return <div 
+      tabIndex={0}
+      id="app"
+      className="app"
+      >
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/hacker-news" component={HackerNews} />
+        <Route path="/" component={Footer} />
+      </div>
+  }
 }
 
-export default App;
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default withRouter(connect(App));
